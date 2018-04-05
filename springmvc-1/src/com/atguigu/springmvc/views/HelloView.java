871f728bbd777094ee00dab8fd39f9a050b462e1
mszyj <m15829082449@163.com>
2018-04-05 21:32:28 +0800

    first commit

---
+++ springmvc-1/src/com/atguigu/springmvc/views/HelloView.java
@@ -0,0 +1,24 @@
+package com.atguigu.springmvc.views;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.View;
+
+@Component
+public class HelloView implements View{
+
+	@Override
+	public String getContentType() {
+		return "text/html";
+	}
+
+	@Override
+	public void render(Map<String, ?> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		response.getWriter().print("hello view, time: " + new Date());
+	}
+
+}
